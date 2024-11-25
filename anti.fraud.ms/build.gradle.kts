63c3e8552dd7e96b995b3b8cb9ede01180fa428a
Fernando Tanta <gestor_usi01@minedu.gob.pe>
2024-11-25 13:09:05 -0500

    Version inicial

---
+++ anti.fraud.ms/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.4"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "pe.yape"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.apache.kafka:kafka-clients:3.5.0")
+	implementation("org.springframework.kafka:spring-kafka:3.0.6")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	implementation("org.mapstruct:mapstruct:1.5.3.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ transaction.ms/src/test/java/pe/yape/transaction/ms/ApplicationTests.java
@@ -0,0 +1,11 @@
+package pe.yape.transaction.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+
+
+}
